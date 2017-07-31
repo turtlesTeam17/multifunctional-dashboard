fd6a2343f913a39b7aa8ab340f7e4b750540bde2
Puritanic <darktasevski@gmail.com>
2017-08-01 01:56:52 +0200

    Revert "Delete Turtle-icon.png"
    This reverts commit c6da1b3bca511036f25f6d606de832c8ee4d5cb9.

--- background.js
+++ background.js
@@ -1,3 +1,14 @@
-/**
- * Created by Tudor on 7/31/2017.
- */
+/*
+Open a new tab, and load "dashboard.html" into it.
+*/
+function ext() {
+   chrome.tabs.create({
+     "url": "/dashboard.html"
+   });
+}
+
+
+/*
+Add ext() as a listener to clicks on the browser action.
+*/
+chrome.browserAction.onClicked.addListener(ext);
