9f53aa4b4352e3340fe58481a40edde1221e6af3
Puritanic <darktasevski@gmail.com>
2017-08-03 22:45:14 +0200

    Initial version of popup

--- development/app/assets/scripts/modules/readInput.js
+++ development/app/assets/scripts/modules/readInput.js
@@ -1,22 +1,27 @@
 // reads value from input field and shorten it
 
 function readInput() {
-
+    // init variables and API keys
     var login = LOGIN;
     var api_key = API;
     var input;
-    var long_UrlInput;
+    var long_UrlInput = $("#longUrl_input");
 
     // checking if the user input is a valid url
     // https://stackoverflow.com/questions/30970068/js-regex-url-validation
     function isUrlValid(userInput) {
+        // Regex ftw
         var res = userInput.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+       // if not valid, displays error message
         if (res == null) {
             console.log('Invalid input!');
+            var error_text = '<p>Invalid input!</p>';
+            $('.shortUrlInfo').append(error_text);
             return false;
         } else {
+            // otherwise sets input to long url value
             input = userInput;
-            console.log(input);         
+            console.log(input);
             return true;
         }
     }
@@ -30,7 +35,7 @@ function readInput() {
                 "longUrl": longUrl
             },
             function (response) {
-                if(!response)
+                if (!response)
                     console.log('Error happened :(');
                 else
                     func(response.data.url);
@@ -39,32 +44,30 @@ function readInput() {
     }
     // writes original and short url to infobox
     function writeUrls(long, short) {
-        var shortedLong;
-        // Substring displayed url if its longer than 80 chars
-        if (long.length > 80) {
-            shortedLong = long.substring(0, 80) + '...';
-            var longUrl_text = '<p>Original url: <a href="' + long + '">' + shortedLong + '</a></p>';
-        } else {
-            var longUrl_text = '<p>Original url: <a href="' + long + '">' + long + '</a></p>';
+        // checks if there is valid short url(otherwise it would print Short url: undefined)
+        if(short){
+            var shortUrl_text = '<p>Short url: <a href="' + short + '" target="_blank">' + short + '</a></p>';
+            $('.shortUrlInfo').append(shortUrl_text);
         }
-        var shortUrl_text = '<p>Short url: <a href="' + short + '">' + short + '</a></p>';
-        $('.longUrlInfo').append(longUrl_text);
-        $('.shortUrlInfo').append(shortUrl_text);
     }
     // clears long and short infobox urls
     function clearUrls() {
-        $('.longUrlInfo').empty();
         $('.shortUrlInfo').empty();
     }
 
     $('#shortenButton').on('click', function () {
+        // clear infobox
         clearUrls();
-        long_UrlInput = $("#longUrl_input");
+        // check if valid url 
         isUrlValid(long_UrlInput.val());
+        // then shorten it
         get_short_url(input, login, api_key, function (short_url) {
             console.log(short_url);
             writeUrls(input, short_url);
         });
+        // reset input value
+        input = '';
+        // reset longurl value
         long_UrlInput.val('');
     })
 }
