45b5336f19d120fede201227a89b3edbe1e5f74a
Darkø Tasevski <darktasevski@gmail.com>
2017-08-11 19:55:32 +0200

    Update App.js

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,11 +1,9 @@
 import $ from './vendor/jquery-3.2.1.min';
 
-// import readInput from './modules/readInput';
 import shortenTabUrl from './modules/shortenTabUrl';
 import './vendor/jquery.qrcode.min';
 import printPalette from './modules/getPalette';
 import { storeColorPickerData, printNewHistoryColor, printHistoryColor } from './modules/colorHistory';
-// readInput();
 shortenTabUrl();
 
 $(document).ready(function() {
